44825beec6278c1cf60b22cbdf30ffac057edc7a
c4m3lion <86702015+c4m3lion@users.noreply.github.com>
2022-04-29 12:34:16 +0300

    assets added and accordion

---
+++ lib/components/accordion.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+const double _kPanelHeaderCollapsedHeight = kMinInteractiveDimension;
+const EdgeInsets _kPanelHeaderExpandedDefaultPadding = EdgeInsets.symmetric(
+  vertical: 64.0 - _kPanelHeaderCollapsedHeight,
+);
+
+class SdxAccordion extends StatelessWidget {
+  const SdxAccordion({
+    Key? key,
+    this.children = const <ExpansionPanel>[],
+    this.expansionCallback,
+    this.animationDuration = kThemeAnimationDuration,
+    this.expandedHeaderPadding = _kPanelHeaderExpandedDefaultPadding,
+    this.dividerColor,
+    this.elevation = 2,
+  })  : assert(children != null),
+        assert(animationDuration != null),
+        _allowOnlyOnePanelOpen = false,
+        initialOpenPanelValue = null,
+        super(key: key);
+
+  /// The children of the expansion panel list. They are laid out in a similar
+  /// fashion to [ListBody].
+  final List<ExpansionPanel> children;
+
+  /// The callback that gets called whenever one of the expand/collapse buttons
+  /// is pressed. The arguments passed to the callback are the index of the
+  /// pressed panel and whether the panel is currently expanded or not.
+  ///
+  /// If ExpansionPanelList.radio is used, the callback may be called a
+  /// second time if a different panel was previously open. The arguments
+  /// passed to the second callback are the index of the panel that will close
+  /// and false, marking that it will be closed.
+  ///
+  /// For ExpansionPanelList, the callback needs to setState when it's notified
+  /// about the closing/opening panel. On the other hand, the callback for
+  /// ExpansionPanelList.radio is simply meant to inform the parent widget of
+  /// changes, as the radio panels' open/close states are managed internally.
+  ///
+  /// This callback is useful in order to keep track of the expanded/collapsed
+  /// panels in a parent widget that may need to react to these changes.
+  final ExpansionPanelCallback? expansionCallback;
+
+  /// The duration of the expansion animation.
+  final Duration animationDuration;
+
+  // Whether multiple panels can be open simultaneously
+  final bool _allowOnlyOnePanelOpen;
+
+  /// The value of the panel that initially begins open. (This value is
+  /// only used when initializing with the [ExpansionPanelList.radio]
+  /// constructor.)
+  final Object? initialOpenPanelValue;
+
+  /// The padding that surrounds the panel header when expanded.
+  ///
+  /// By default, 16px of space is added to the header vertically (above and below)
+  /// during expansion.
+  final EdgeInsets expandedHeaderPadding;
+
+  /// Defines color for the divider when [ExpansionPanel.isExpanded] is false.
+  ///
+  /// If `dividerColor` is null, then [DividerThemeData.color] is used. If that
+  /// is null, then [ThemeData.dividerColor] is used.
+  final Color? dividerColor;
+
+  /// Defines elevation for the [ExpansionPanel] while it's expanded.
+  ///
+  /// By default, the value of elevation is 2.
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionPanelList(
+      children: children,
+      expansionCallback: expansionCallback,
+      animationDuration: animationDuration,
+      expandedHeaderPadding: expandedHeaderPadding,
+      dividerColor: dividerColor,
+      elevation: elevation,
+    );
+  }
+}
---
+++ lib/datas/sdx_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color currentSection = Color(0x62626200);
+  static const Color aBackground = Color(0xE5E5E500);
+  static const Color aSidebar = Color(0x7C01FF00);
+  static const Color aSoundButton = Color(0xA93EF000);
+  static const Color aSoundLockedButton = Color(0x55555500);
+  static const Color aSoundButtonText = Color(0xFFFFFF00);
+}
